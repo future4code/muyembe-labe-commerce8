b97171498b0ddafed643f0cef5440dc269517ea8
JoaoTrajano <joao.trajanosouza@gmail.com>
2021-01-23 01:20:22 -0300

    botao adicionar carrinho

---
+++ src/components/BotaoAdicionarCarrinho.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class BotaoAdicionarCarrinho extends React.Component {
+
+    render() {
+        return (
+            <div className="botao-add-carrinho">
+                <button>
+                    <div>
+                        <span> Adicionar ao carrinho.</span>
+                    </div>
+                    <div>
+                        <img src="https://images.vexels.com/media/users/3/200097/isolated/preview/942820836246f08c2d6be20a45a84139-carrinho-de-compras-icon-carrinho-de-compras-by-vexels.png" width="30px" />
+                    </div>
+                </button>
+            </div>
+        );
+    }
+}
+
+export default BotaoAdicionarCarrinho
